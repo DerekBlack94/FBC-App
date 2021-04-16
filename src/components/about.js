4df3118b85da92cf6b4d440aac19e6dfe2ffc47d
Derek Black <derekblack94@yahoo.com>
2021-04-16 11:58:18 -0400

    changed procfile

--- src/components/about.js
+++ src/components/about.js
@@ -11,7 +11,7 @@ class About extends Component{
                   <aside className="container">
                     <div className="col-sm aboutUs">
                         <h2 className="mcell-title story">Family bike chain</h2>
-                        <div className="padding20">@mrecoder</div>
+                        <div className="padding20">@derekblack</div>
                     </div>
                   </aside>
             </div>
